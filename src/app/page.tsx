e13d3cf5333b8c2fb8d31e04359e3c678a04e4c7
Oscar Sánchez <5068406+osanchezroca@users.noreply.github.com>
2024-05-03 00:19:05 +0200

    fix: cleaning code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+'use client'
+import Spinner from "@src/components/Spinner";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   //redirect to home page
   const router = useRouter();
-  router.push("/home");
+  useEffect(() => {
+    router.push("/home");
+  }, []);
+  
   return (
-    <div>
-      Loading
+    <div className="space-x-3">
+      <Spinner />Loading
     </div>
   );
 }
